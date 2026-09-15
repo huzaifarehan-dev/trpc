eff14bf255a1c66697ca0e48e7800ef24593309c
huzaifarehan-dev <huzaifarehan002@gmail.com>
2026-09-15 15:57:58 +0500

    chore: finalize-recheck proof (step 2) for Merge Lens story 3-3

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,3 +2,4 @@ export * from './@trpc/server';
 
 // merge-lens incremental proof 2026-09-15T08:36:04.425Z
 // merge-lens divergence proof (step 1) 2026-09-15T10:57:48.214Z
+// merge-lens finalize-recheck proof (step 2) 2026-09-15T10:57:58.303Z
